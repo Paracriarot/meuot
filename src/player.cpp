632ac3f60b15e63709602cb8b13035da9ee48285
Paracriarot <78766248+Paracriarot@users.noreply.github.com>
2021-03-24 16:12:01 -0300

    Add files via upload

--- src/player.cpp
+++ src/player.cpp
@@ -111,7 +111,7 @@ std::string Player::getDescription(int32_t lookDistance) const
 	if (lookDistance == -1) {
 
                 int32_t value;
-		s << "você. (Level " << level << ") [Reset " << getStorageValue(STORAGEVALUE_RESETS, value) << "] {Idade 0}.";
+		s << "você. (Level " << level << ") [Reset " << getStorageValue(5123513,value) << "] {Idade 0}.";
 
 		if (group->access) {
 			s << " Você é um " << group->name << '.';
