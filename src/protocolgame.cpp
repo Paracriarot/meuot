97d48ea8b0a5ec4dda4918896dd0b7ac4a0941e7
Paracriarot <78766248+Paracriarot@users.noreply.github.com>
2021-03-25 21:09:24 -0300

    Add files via upload

--- src/protocolgame.cpp
+++ src/protocolgame.cpp
@@ -3492,9 +3492,10 @@ void ProtocolGame::AddPlayerStats(NetworkMessage& msg)
 {
 	msg.addByte(0xA0);
 
+    if (player->getMaxHealth() > 65535) {
     msg.add<uint16_t>(std::min<int32_t>(player->getHealth(), std::numeric_limits<uint16_t>::max()));
     msg.add<uint16_t>(std::min<int32_t>(player->getMaxHealth(), std::numeric_limits<uint16_t>::max()));
-
+}
     if (player->getMaxHealth() > 65535) {
         msg.add<uint16_t>(std::min<int32_t>(player->getHealth() * 100 / player->getMaxHealth(), std::numeric_limits<uint16_t>::max()));
         msg.add<uint16_t>(100);
@@ -3523,8 +3524,10 @@ void ProtocolGame::AddPlayerStats(NetworkMessage& msg)
 	msg.add<uint16_t>(player->getStoreXpBoost()); // xp boost
 	msg.add<uint16_t>(player->getStaminaXpBoost()); // stamina multiplier (100 = 1.0x)
 
+    if (player->getMaxMana() < 65535) {
     msg.add<uint16_t>(std::min<int32_t>(player->getMana(), std::numeric_limits<uint16_t>::max()));
     msg.add<uint16_t>(std::min<int32_t>(player->getMaxMana(), std::numeric_limits<uint16_t>::max()));
+}
 
     if (player->getMaxMana() > 65535) {
         msg.add<uint16_t>(std::min<int32_t>(player->getMana() * 100 / player->getMaxMana(), std::numeric_limits<uint16_t>::max()));
